7129618c1d36fe5340e5c71c9284e4c339fff713
LIMT34 <91453257+LIMT34@users.noreply.github.com>
2022-02-22 20:06:33 +0900

    Add file for Ubuntu version
    Corrected the name of the weight file to the same name as the Windows version.

---
+++ LIMTrackService/matterport/fix_model_ubuntu.py
@@ -0,0 +1,23 @@
+﻿import shutil
+import os
+file_name = os.environ['HOME'] + "/LIMTrackService/matterport/mrcnn/model.py"
+back_name = file_name + ".original"
+shutil.copy(file_name, back_name)
+
+with open(file_name) as f:
+    data_lines = f.read()
+
+data_lines = data_lines.replace("checkpoints = filter(lambda f: f.startswith(\"mask_rcnn\"), checkpoints)", "checkpoints = filter(lambda f: f.startswith(\"weight\"), checkpoints)")
+data_lines = data_lines.replace("regex = r\".*[/\\\\][\\w-]+(\\d{4})(\\d{2})(\\d{2})T(\\d{2})(\\d{2})[/\\\\]mask\\_rcnn\\_[\\w-]+(\\d{4})\\.h5\"", "regex = r\".*[/\\\\][\\w-]+(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})[/\\\\]weight\\_(\\d{4})\\.h5\"")
+data_lines = data_lines.replace("self.log_dir = os.path.join(self.model_dir, \"{}{:%Y%m%dT%H%M}\".format(", "self.log_dir = os.path.join(self.model_dir, \"weight_{:%Y%m%d%H%M}\".format(now))")
+data_lines = data_lines.replace("self.config.NAME.lower(), now))", "")
+data_lines = data_lines.replace("self.checkpoint_path = os.path.join(self.log_dir, \"mask_rcnn_{}_*epoch*.h5\".format(", "self.checkpoint_path = os.path.join(self.log_dir, \"weight_*epoch*.h5\")")
+data_lines = data_lines.replace("self.config.NAME.lower()))", "")
+data_lines = data_lines.replace("callbacks += custom_callbacks", "callbacks = custom_callbacks")
+data_lines = data_lines.replace("max_queue_size=100,", "max_queue_size=2,")
+
+with open(file_name, mode="w") as f:
+    f.write(data_lines)
+
+#python ~/LIMTrackService/matterport/fix_model_ubuntu.py
+
